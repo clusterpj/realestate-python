659c9817a6e3b57be25f95fc0bc50330c896c931
Pedro Jimenez <jisgore@gmail.com>
2023-04-14 19:02:39 -0400

    Many Things haver changed

--- app.py
+++ app.py
@@ -1,10 +1,35 @@
 from flask import Flask, render_template
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from flask import Flask, render_template, request, redirect, url_for, flash
+from flask_login import LoginManager, UserMixin, login_required, login_user, logout_user, current_user
+from werkzeug.security import check_password_hash
+from werkzeug.security import generate_password_hash
+from flask_login import logout_user
 
 app = Flask(__name__)
-app.config["MONGO_URI"] = "mongodb://localhost:27017/realestate"
+app.secret_key = "dTLma3M6KkGrDf"
+
+app.config["MONGO_URI"] = "mongodb://localhost:27017/cluster_main"
 mongo = PyMongo(app)
+
+# Set up Flask-Login
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = "login"
+
+class User(UserMixin):
+    def __init__(self, user_data):
+        self.id = str(user_data['_id'])
+        self.username = user_data['username']
+
+@login_manager.user_loader
+def load_user(user_id):
+    # Load user from the database or user data store
+    user_data = mongo.db.users.find_one({'_id': ObjectId(user_id)})
+    if user_data:
+        user = User(user_data)
+        return user
 
 @app.route('/')
 def home(): 
@@ -34,13 +59,82 @@ def news():
 def contact():
     return render_template('contact.html', title='Real Estate App')
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('login.html', title='Real Estate App')
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
 
-@app.route('/signup')
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user_data = mongo.db.users.find_one({'username': username})
+
+        if user_data and check_password_hash(user_data['password'], password):
+            user = User(user_data)
+            login_user(user)
+            next_page = request.args.get('next')
+            return redirect(next_page or url_for('home'))
+
+        flash('Invalid username or password.', 'danger')
+
+    return render_template('login.html')
+
+@app.route('/signup', methods=['GET', 'POST'])
 def signup():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        # Check if the username already exists
+        existing_user = mongo.db.users.find_one({'username': username})
+
+        if existing_user is None:
+            # Hash the password
+            hashed_password = generate_password_hash(password)
+            # Add the new user to the database
+            mongo.db.users.insert_one({'username': username, 'password': hashed_password})
+            flash('Account created successfully. Please log in.', 'success')
+            return redirect(url_for('login'))
+        else:
+            flash('A user with that username already exists.', 'danger')
+
     return render_template('signup.html', title='Real Estate App')
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
+@app.route('/admin')
+@login_required
+def admin_dashboard():
+    return render_template('admin_dashboard.html')
+
+@app.route('/admin/all_properties')
+@login_required
+def all_properties():
+    # Fetch all properties and render the all_properties page
+    return render_template('admin/all_properties.html')
+
+@app.route('/admin/property_types')
+@login_required
+def property_types():
+    # Fetch all property types and render the property_types page
+    return render_template('admin/property_types.html')
+
+@app.route('/admin/property_amenities')
+@login_required
+def property_amenities():
+    # Fetch all property amenities and render the property_amenities page
+    return render_template('admin/property_amenities.html')
+
+@app.route('/admin/property_locations')
+@login_required
+def property_locations():
+    # Fetch all property locations and render the property_locations page
+    return render_template('admin/property_locations.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
